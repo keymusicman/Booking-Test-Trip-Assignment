7b1884b55e87230b904ce8613328ee1919a4415d
Vasily Maleev <keymusicman@gmail.com>
2022-04-09 16:09:44 +0400

    Show fragment

--- app/src/main/java/com/booking/tripsassignment/MainActivity.kt
+++ app/src/main/java/com/booking/tripsassignment/MainActivity.kt
@@ -2,10 +2,15 @@ package com.booking.tripsassignment
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.booking.tripsassignment.feature.trips.TripsFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_screen)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, TripsFragment.newInstance())
+            .commit()
     }
 }
--- app/src/main/java/com/booking/tripsassignment/feature/trips/TripsFragment.kt
+++ app/src/main/java/com/booking/tripsassignment/feature/trips/TripsFragment.kt
@@ -37,4 +37,8 @@ class TripsFragment : Fragment(R.layout.trips_list_screen) {
             }
         }
     }
+
+    companion object {
+        fun newInstance() = TripsFragment()
+    }
 }
