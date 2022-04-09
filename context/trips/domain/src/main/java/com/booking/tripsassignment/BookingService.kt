482dab6c062c713efdbfabdb311a013b0fdf5a8e
Vasily Maleev <keymusicman@gmail.com>
2022-04-09 18:45:05 +0400

    Add comments

--- context/trips/domain/src/main/java/com/booking/tripsassignment/BookingService.kt
+++ context/trips/domain/src/main/java/com/booking/tripsassignment/BookingService.kt
@@ -2,9 +2,17 @@ package com.booking.tripsassignment
 
 import org.joda.time.LocalDate
 
+/**
+ * Service that allows operating bookings, its chains, etc
+ */
 class BookingService(
     private val repository: BookingRepository
 ) {
+    /**
+     * Gets all the chains that belong to a [userId]. Bookings inside chain
+     * are considered sorted by checkin date ascending and chains itself are
+     * sorted by checkin date
+     */
     fun getAllChains(userId: Int): List<List<Booking>> {
         return when (val result = repository.fetchBookings(userId)) {
             is Result.Success -> result.data.toChains()
@@ -12,6 +20,9 @@ class BookingService(
         }
     }
 
+    /**
+     * Split all the bookings into chains
+     */
     private fun List<Booking>.toChains(): List<List<Booking>> {
         if (isEmpty()) return emptyList()
 
